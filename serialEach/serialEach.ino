d596694fdacd447c3091e343cf189a7954f26053
smart-life-tech <christlightworld@gmail.com>
2023-11-08 08:57:20 +0100

    new file:   serialEach/serialEach.ino

---
+++ serialEach/serialEach.ino
@@ -0,0 +1,25 @@
+// this code uses all the pins to test from 2 to 13, observe the working pattern please
+void setup(void)
+{
+    Serial.begin(9600);
+    for (size_t i = 0; i < 5; i++)
+    {
+        pinMode(i, OUTPUT);
+    }
+}
+
+void loop(void)
+{
+
+    //====================================
+    // blink the n led pin only
+    for (size_t i = 2; i < 14; i++)
+    {
+        Serial.println("now on  pin ," + String(i));
+        delay(1800);
+        digitalWrite(i, HIGH);
+        Serial.println("done with pin ," + String(i));
+        digitalWrite(i, LOW);
+        delay(1000);
+    }
+}
